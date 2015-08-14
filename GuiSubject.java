7f389e05e3f41370269c609c8790404cee159026
kslsruti <srutiksl@gmail.com>
2015-08-14 11:45:25 -0700

    gui subject

--- GuiSubject.java
+++ GuiSubject.java
@@ -36,8 +36,8 @@ public class GuiSubject {
         candies = null;
         beverages = null;
         Requirements r = new CalorieReq(this);
-        Requirements r1 = new FatReq(this);
-        Requirements r2 = new SugarReq(this);
+        Requirements r1 = new SugarReq(this);
+        Requirements r2 = new FatReq(this);
         Requirements r3 = new PriceReq(this);
         
     }
